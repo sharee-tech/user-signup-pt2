87a570da686a35071a5e5039c7082411a33b139d
sharee-tech <mail.sharee@gmail.com>
2023-10-10 09:53:25 -0500

    completed studio

---
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -0,0 +1,55 @@
+package org.launchcode.spaday.models;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+public class User {
+    @NotBlank(message = "Valid username required.")
+    @Size(min = 5, max = 15, message = "Username must be between 5 and 15 characters.")
+    private String username;
+
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotEmpty(message = "Password is required.")
+    @Size(min = 6, message = "Password must be at least 6 characters.")
+    private String password;
+
+    public User() {
+
+    }
+
+    public User(String username, String email, String password) {
+        this();
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
